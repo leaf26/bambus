aa8f6da52518efcae78a4633318b385ee95fe0cc
René Klomp <rene@rene-E6220.(none)>
2013-01-31 11:08:34 +0100

    Incorrect year

--- src/js/tsunami.js
+++ src/js/tsunami.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, René Klomp, Edwin Schaap
+ * Copyright (c) 2013, René Klomp, Edwin Schaap
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
